943b64441b373bff064fd0836b36db796180bfe7
Alex-G-R <alex.grabski555@gmail.com>
2026-03-13 22:24:57 +0100

    Create a window, incorporate FRSGUI

--- main.cpp
+++ main.cpp
@@ -3,13 +3,40 @@
 
 int main()
 {
-    auto lang = "FRSGUI";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    /* Initialize SFML redner window, create a pointer to window */
+    constexpr int W_WIDTH = 800;
+    constexpr int W_HEIGHT = 600;
+    const auto WINDOW = std::make_shared<sf::RenderWindow>(sf::VideoMode({W_WIDTH, W_HEIGHT}), "Dude-sim");
 
-    for (int i = 1; i <= 5; i++)
+    /* init FRSGUI */
+    fr::FRSGUI FRSGUI(WINDOW);
+
+
+    /* SFML render loop */
+    while (WINDOW->isOpen())
     {
-        std::cout << "i = " << i << std::endl;`
+        /* Pool events, if event = close, break the loop if any other pass it to the FRSGUI to handle*/
+        while (const std::optional<sf::Event> event = WINDOW->pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                WINDOW->close();
+                break;
+            }
+            FRSGUI.dispatchEvent(event);
+        }
+        WINDOW->clear();
+
+        /* rendering */
+
+
+
+        /* Render GUI by FRSGUI */
+        FRSGUI.Render();
+        /* SFML display function */
+        WINDOW->display();
     }
+
 
     return 0;
 }
