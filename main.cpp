84143659f835805c2fbfc7b51f5910c250554e3c
Alex-G-R <alex.grabski555@gmail.com>
2026-03-13 23:22:01 +0100

    Add banner

--- main.cpp
+++ main.cpp
@@ -6,10 +6,29 @@ int main()
     /* Initialize SFML redner window, create a pointer to window */
     constexpr int W_WIDTH = 800;
     constexpr int W_HEIGHT = 600;
-    const auto WINDOW = std::make_shared<sf::RenderWindow>(sf::VideoMode({W_WIDTH, W_HEIGHT}), "Dude-sim");
+    const auto WINDOW = std::make_shared<sf::RenderWindow>(
+        sf::VideoMode({W_WIDTH, W_HEIGHT}),
+        "Dude-sim | Alex Grabski"
+        );
 
     /* init FRSGUI */
     fr::FRSGUI FRSGUI(WINDOW);
+
+
+    /* Welcome text and start button */
+    const auto banner = fr::UIElement::Builder(&FRSGUI)
+    .setID("Banner")
+    .addGroups({"Banner_main", "scene_one"})
+    .setText("Dude-sim")
+    .buildUIElement();
+    // Create style
+    auto banner_main_style = Style("Banner_main", fr::ApplyBy::GROUP, 1, {
+            {fr::KEY::POSITION, sf::Vector2f(W_WIDTH/2 - W_WIDTH/2 , 0.f)},
+            {fr::KEY::SIZE, sf::Vector2f(W_WIDTH, W_HEIGHT/12)},
+            {fr::KEY::BACKGROUND_COLOR, sf::Color{255, 255, 255, 255}},
+            {fr::KEY::CHARACTER_SIZE, 36},
+            {fr::KEY::TEXT_COLOR, sf::Color::Black}
+    }, FRSGUI);
 
 
     /* SFML render loop */
